71e0085ffaaf2a310bfa7fb6f7cb11b77f5ae3fc
Haibin Zhang <haibinzh@br006.pvt.bridges.psc.edu>
2017-10-27 17:00:23 -0400

    new

---
+++ hw2/submit.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p RM
+#SBATCH --ntasks-per-node 28
+#SBATCH -t 00:05:00
+# echo commands to stdout
+set -x
+
+# run OpenMP program
+export OMP_NUM_THREAD=28
+./main.out
+

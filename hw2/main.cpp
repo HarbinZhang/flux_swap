782035fcc955a0df30e955a3f9b1d33f027f560f
HarbinZhang <seashore.real@gmail.com>
2017-10-27 21:50:05 -0400

    hell

--- hw2/main.cpp
+++ hw2/main.cpp
@@ -5,20 +5,21 @@
 #include <stack>
 #include <utility>
 #include "g.h"
-#include "Myqueue.h"
+#include <queue>
+// #include "Myqueue.h"
 using namespace std;
 
 
 static const int s = 12;
-static const double fai = 1e-6;
+static const double fai = 1e-2;
 
 
 int main(){
 
-	Myqueue q;
+	queue<pair<double, double> > q;
 	double res = 0;
 	double M = 0;
-	q.push(make_pair(1.0, 100.0));
+	q.push(make_pair(1.0, 5.0));
 
 	omp_lock_t lock;
 	omp_lock_t lock_q;
@@ -30,13 +31,14 @@ int main(){
 	{
 		double local_M = 0;
 		stack<pair <double, double> > stack;
-		// omp_set_lock(&lock);
+		omp_set_lock(&lock_q);
 		while(q.size() > 0){
 			// omp_unset_lock(&lock);
-			pair<double, double> temp = q.pop();
-			// omp_unset_lock(&lock);
+			pair<double, double> temp = q.front();
+			q.pop();
+			omp_unset_lock(&lock_q);
 
-			if(temp.second == 0){continue;}
+			// if(temp.second == 0){continue;}
 
 			// omp_set_lock(&lock);
 			// cout<<"temp: "<<temp.first<<" "<<temp.second<<endl;
@@ -45,12 +47,14 @@ int main(){
 			double a = temp.first;
 			double b = temp.second;
 
+			double ga = g(a);
+			double gb = g(b);
+
 			omp_set_lock(&lock);
-			M = max(M, g(a));
-			M = max(M, g(b));
+			M = max(M, ga);
+			M = max(M, gb);
 			// cout<<"new M: "<<M<<" a: "<<a<<" b: "<<b<<endl;
 			omp_unset_lock(&lock);
-
 
 			double newg = (g(a) + g(b) + s*(b-a)) / 2;
 
@@ -64,23 +68,22 @@ int main(){
 				// cout<<"a+b: "<<a+b<<endl;
 				omp_unset_lock(&lock);
 
+				omp_set_lock(&lock_q);
 				q.push(make_pair (a, (a+b)/2));
 				q.push(make_pair ((a+b)/2, b));
-				// omp_unset_lock(&lock);
+				omp_unset_lock(&lock_q);
 			}
 		}
-		// omp_unset_lock(&lock);
+		omp_unset_lock(&lock_q);
 		// omp_set_lock(&lock);
 		// cout<<"M: "<<M<<" with id= "<<omp_get_thread_num()<<endl;
 		// omp_unset_lock(&lock);
-
-		#pragma omp barrier			
+	
 	}
 
 
 	double end = omp_get_wtime();
 	cout<<"M: "<<M<<" duration: "<<end - start<<endl;
-
 	omp_destroy_lock(&lock);
 	omp_destroy_lock(&lock_q);
 

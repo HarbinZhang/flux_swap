64515a3ce2b64ba09ef6f1427661cc9276bfa232
HarbinZhang <seashore.real@gmail.com>
2017-11-08 21:27:48 -0500

    auto

--- atomic.cu
+++ atomic.cu
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include "gputimer.h"
+// #include "gputimer.h"
 
 #define NUM_THREADS 1000000
 #define ARRAY_SIZE  100
@@ -35,7 +35,7 @@ __global__ void increment_atomic(int *g)
 
 int main(int argc,char **argv)
 {   
-    GpuTimer timer;
+    // GpuTimer timer;
     printf("%d total threads in %d blocks writing into %d array elements\n",
            NUM_THREADS, NUM_THREADS / BLOCK_WIDTH, ARRAY_SIZE);
 
@@ -49,7 +49,7 @@ int main(int argc,char **argv)
     cudaMemset((void *) d_array, 0, ARRAY_BYTES); 
 
     // launch the kernel - comment out one of these
-    timer.Start();
+    // timer.Start();
     
     // Instructions: This program is needed for the next quiz
     // uncomment increment_naive to measure speed and accuracy 
@@ -57,12 +57,12 @@ int main(int argc,char **argv)
     // measure speed and accuracy of  atomic icrements
     // increment_naive<<<NUM_THREADS/BLOCK_WIDTH, BLOCK_WIDTH>>>(d_array);
     increment_atomic<<<NUM_THREADS/BLOCK_WIDTH, BLOCK_WIDTH>>>(d_array);
-    timer.Stop();
+    // timer.Stop();
     
     // copy back the array of sums from GPU and print
     cudaMemcpy(h_array, d_array, ARRAY_BYTES, cudaMemcpyDeviceToHost);
     print_array(h_array, ARRAY_SIZE);
-    printf("Time elapsed = %g ms\n", timer.Elapsed());
+    // printf("Time elapsed = %g ms\n", timer.Elapsed());
  
     // free GPU memory allocation and exit
     cudaFree(d_array);

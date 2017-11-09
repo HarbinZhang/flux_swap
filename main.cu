1047507c20524764d565db866941cb57dcda297c
HarbinZhang <seashore.real@gmail.com>
2017-11-08 21:18:41 -0500

    auto

--- main.cu
+++ main.cu
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-__global__ void cube(float * d_out, float * d_in){
+__global__ void square(float * d_out, float * d_in){
 	// Todo: Fill in this function
   int index = threadIdx.x;
-  d_out[index] = d_in[index] * d_in[index] * d_in[index];
+  d_out[index] = d_in[index] * d_in[index];
 }
 
 int main(int argc, char ** argv) {
@@ -26,13 +26,13 @@ int main(int argc, char ** argv) {
 	cudaMalloc((void**) &d_out, ARRAY_BYTES);
 
 	// transfer the array to the GPU
-	cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
+	// cudaMemcpy(d_in, h_in, ARRAY_BYTES, cudaMemcpyHostToDevice);
 
 	// launch the kernel
-	cube<<<1, ARRAY_SIZE>>>(d_out, d_in);
+	// square<<<1, ARRAY_SIZE>>>(d_out, d_in);
 
 	// copy back the result array to the CPU
-	cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
+	// cudaMemcpy(h_out, d_out, ARRAY_BYTES, cudaMemcpyDeviceToHost);
 
 	// print out the resulting array
 	for (int i =0; i < ARRAY_SIZE; i++) {
